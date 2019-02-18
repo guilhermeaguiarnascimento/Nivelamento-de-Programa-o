bb04cf5970b6f7dc611e614bbadffa663fc4f2f4
guilhermeaguiarnascimento <nascimento.guilherme.aguiar@gmail.com>
2019-02-18 00:14:00 -0300

    POO Exe - Carro Finalizado

--- src/poo/Program.java
+++ src/poo/Program.java
@@ -1,27 +1,31 @@
 package poo;
 
 public class Program extends Motorista {
-	
+
 	public static void main(String[] args) {
-		
+
 		Carro c1 = new Carro();
 		c1.setChassi("GolG90");
 		c1.setPlaca("Gui2014");
 		c1.setCor("Branca");
 		c1.setModelo("Quadrado");
-	
-		Motorista m1 = new Motorista ();
+
+		Motorista m1 = new Motorista();
 		m1.setNome("Bine");
 		m1.setCpf("53472457672");
 		m1.setIdentidade(871776);
 		m1.setIdade(34);
-		m1.setSexo("Femenino");
+		m1.setSexo("Feminino");
 		m1.setCarro(c1);
-		
-		
-		System.out.println(m1);
-	}
-	
-	
 
+		System.out.println("Motorista: " + m1.getNome());
+		System.out.println("CPF: " + m1.getCpf());
+		System.out.println("Identidade: " + m1.getIdentidade());
+		System.out.println("Idade: " + m1.getIdade());
+		System.out.println("Sexo: " + m1.getSexo());
+		System.out.println("Chassi: " + m1.getCarro().getChassi());
+		System.out.println("Placa: " + m1.getCarro().getPlaca());
+		System.out.println("Cor: " + m1.getCarro().getCor());
+		System.out.println("Modelo: " + m1.getCarro().getModelo());
+	}
 }
